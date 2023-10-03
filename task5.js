7ad80c4c19c248636236d91ffb60907fa4645044
Orkhan <mammedov.01@mail.ru>
2023-10-03 21:43:17 +0400

    task5

---
+++ task5.js
@@ -0,0 +1,53 @@
+let attempts = 3;
+
+reg: while (true) {
+  alert("Please register");
+  let regLogin = prompt("Create your login");
+  let regPass = prompt("Create your password");
+  alert("Please auth");
+
+  auth: while (true) {
+    let authLogin = prompt("Type your login");
+    let authPass = prompt("Type your password");
+
+    if (authLogin === regLogin && authPass === regPass) {
+      alert("It's ok");
+      break reg;
+    } else {
+      alert("Login or password is incorrect");
+      --attempts;
+    }
+    if (attempts <= 0) {
+      let createNewUser = confirm("Do you want to create a new account?");
+      if (createNewUser === true) {
+        attempts = 3;
+        continue reg;
+      } else if (createNewUser === false) {
+        createNewUser = confirm("Do you want to reset your password?");
+        if (createNewUser === false) {
+          attempts = 3;
+          continue auth;
+        } else if (createNewUser === true) {
+          reset: while (true) {
+            let checkLogin = prompt("To reset your password, type your login");
+            if (checkLogin === regLogin) {
+              let newPass = prompt("Create new password");
+              regPass = newPass;
+              continue auth;
+            } else if (checkLogin !== regLogin) {
+              let confirmation = confirm(
+                "Login is incorrect, do you want to create a new account?"
+              );
+              if (confirmation === true) {
+                attempts = 3;
+                continue reg;
+              } else if (confirmation === false) {
+                continue reset;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
