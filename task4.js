b2f6c62e30e0a06b09f78791b03f3e869a768967
Orkhan <mammedov.01@mail.ru>
2023-10-03 19:31:27 +0400

    task4 done

--- task4.js
+++ task4.js
@@ -1,18 +1,31 @@
+let attempts = 3;
+
 reg: while (true) {
-    alert("Please register");
-    let regLogin = prompt("Create your login");
-    let regPass = prompt("Create your password");
-  
-    auth: while (true) {
-      alert("Please auth");
-      let authLogin = prompt("Type your login");
-      let authPass = prompt("Type your password");
-  
-      if (authLogin === regLogin && authPass === regPass) {
-        alert("Its ok");
-        break reg;
-      } else {
-        alert("login or password is incorrect");
+  alert("Please register");
+  let regLogin = prompt("Create your login");
+  let regPass = prompt("Create your password");
+  alert("Please auth");
+
+  auth: while (true) {
+    let authLogin = prompt("Type your login");
+    let authPass = prompt("Type your password");
+
+    if (authLogin === regLogin && authPass === regPass) {
+      alert("Its ok");
+      break reg;
+    } else {
+      alert("login or password is incorrect");
+      --attempts;
+    }
+    if (attempts <= 0) {
+      let createNewUser = confirm("Do you want to create new account?");
+      if (createNewUser === true) {
+        attempts = 3;
+        continue reg;
+      } else if (createNewUser === false) {
+        attempts = 3;
+        continue auth;
       }
     }
   }
+}
